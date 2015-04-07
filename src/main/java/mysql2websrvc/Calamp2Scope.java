b5fde5235abf322153bb34764a9a96b6bb29195c
diego <warlocko.py@gmail.com>
2015-04-07 17:29:10 -0400

    Se inicia programacion para pasar datos calamp a compatibilidad scope.

---
+++ src/main/java/mysql2websrvc/Calamp2Scope.java
@@ -0,0 +1,16 @@
+package mysql2websrvc;
+
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+
+public class Calamp2Scope {
+public static void Migrate(ArrayList <DataObject> datos) {
+	Jsonprototype j = new Jsonprototype ();
+	Gson gson = new Gson ();
+	
+	for (int i=0; i < datos.size(); i++){
+		
+	}
+}
+}
