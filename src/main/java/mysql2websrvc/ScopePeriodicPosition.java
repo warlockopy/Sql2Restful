76f3c0a17e761e2c664167fe17d186b3f19a0eb7
Hugo <crazydarkmaster@gmai.com>
2015-04-08 12:03:19 -0400

    Evento desconocido

---
+++ src/main/java/mysql2websrvc/ScopePeriodicPosition.java
@@ -0,0 +1,12 @@
+package mysql2websrvc;
+
+public class ScopePeriodicPosition {
+	public ScopeEventHeader header;
+	public int rpm = 0;
+	public int trip_duration_seconds;
+	public int trip_distance_meters;
+	
+	public ScopePeriodicPosition (){
+		header = new ScopeEventHeader ();
+	}
+}
