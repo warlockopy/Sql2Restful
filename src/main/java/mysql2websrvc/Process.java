73e4461c4fe4b7135d92b7d584fb811593f40539
Hugo <crazydarkmaster@gmai.com>
2015-08-12 10:55:22 -0400

    MySQL copy test

--- src/main/java/mysql2websrvc/Process.java
+++ src/main/java/mysql2websrvc/Process.java
@@ -68,6 +68,18 @@ public class Process extends Thread{
 							ReadJsonFromMySql.copy (eventId);
 						}
 					}
+					
+					//Test
+					for (int i = 0; i < serverResponse.size (); ++i){
+						BigInteger eventId = ReadJsonFromMySql.getEventIdAt(i);
+						
+						if (eventId.equals ("5294000")){
+							System.err.println ("Special SQL COPY EVENT ID " + eventId);
+							ReadJsonFromMySql.copy (eventId);
+						}
+					}
+					//End test
+					
 				} catch (IOException e) {
 					e.printStackTrace();
 				} catch (ParseException e) {
